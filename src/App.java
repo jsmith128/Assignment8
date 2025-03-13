b12e655f736a035bc0a2013cce0a8cc7baae2fac
jonathan <70296489+jsmith128@users.noreply.github.com>
2025-03-13 16:03:24 -0400

    code

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        try {
+            int input = Integer.parseInt(args[0]);
+            System.out.println(fibonacci(input));
+        } catch (NumberFormatException e) {
+            System.out.println("There was an error parsing your input");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("You must provide a command-line integer argument");
+        }
+    }
+
+    static int fibonacci(int n) {
+        if (n <= 1) return n;
+        
+        return fibonacci(n-1) + fibonacci(n-2);
     }
 }
